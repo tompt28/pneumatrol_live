e6f90bfaf3630a41508436047fe96d606fdd8107
tompt28 <tom_thornber@yahoo.co.uk>
2021-12-07 11:58:01 +0000

    email tests smtp backend

--- ORWAapp/views.py
+++ ORWAapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from ORWAapp.forms import *
 from django.db.models import Q
-from ORWAapp.emails import * 
 from ORWAapp.models import Customers, SalesOrder, Parts, PartType
 from django.http import Http404, HttpResponseRedirect,HttpResponse,FileResponse
 from django.urls import reverse
@@ -584,31 +583,31 @@ def Reject(request, order):
     return render(request, 'ORWAapp/home/Rejection.html', context)
 
 
-# def EmailReminder(request):
+def EmailReminder(request):
 
-#     salesdata = SalesOrder.objects.filter(issue_date__isnull=True).filter(reject_date__isnull=True)
-#     userEmails = []
-#     for user in User.objects.all():
-#         userEmails.append(user.email)
+    salesdata = SalesOrder.objects.filter(issue_date__isnull=True).filter(reject_date__isnull=True)
+    userEmails = []
+    for user in User.objects.all():
+        userEmails.append(user.email)
 
-#     context = {
-#     'SalesOrder':salesdata,
-#     }
+    context = {
+    'SalesOrder':salesdata,
+    }
 
-#     subject ='Weekly ORWA list'
-#     sendfrom = settings.EMAIL_HOST_USER
-#     to = ['tomt@pneumatrol.com']
+    subject ='Weekly ORWA list'
+    sendfrom = settings.EMAIL_HOST_USER
+    to = ['tomt@pneumatrol.com']
 
-#     #send_mail(subject,body,sendfrom,to,fail_silently=False,)
+    #send_mail(subject,body,sendfrom,to,fail_silently=False,)
     
-#     text_content = 'see live.pneumatrol.com'
-#     html_content  = render_to_string('ORWAapp/home/EmailReminder.html', context)
+    text_content = 'see live.pneumatrol.com'
+    html_content  = render_to_string('ORWAapp/home/EmailReminder.html', context)
 
-#     msg = EmailMultiAlternatives(subject, text_content, sendfrom, to)
-#     msg.attach_alternative(html_content, "text/html")
-#     msg.send()
-#     return HttpResponseRedirect(reverse('ORWAapp:home'))
-#     #return render(request,'ORWAapp/home/EmailReminder.html',context)
+    msg = EmailMultiAlternatives(subject, text_content, sendfrom, to)
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
+    return HttpResponseRedirect(reverse('ORWAapp:home'))
+    #return render(request,'ORWAapp/home/EmailReminder.html',context)
 
 def IssueEmail(request, order):
 
