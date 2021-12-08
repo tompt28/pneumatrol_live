ecc6ca44dd0aa42cbb3734fc5b33cd7d1db60751
tompt28 <tom_thornber@yahoo.co.uk>
2021-12-08 11:27:20 +0000

    Reminder email function created

--- ORWAapp/views.py
+++ ORWAapp/views.py
@@ -597,26 +597,11 @@ def IssueEmail(request, order):
 
     salesdata = SalesOrder.objects.get(order_number = order)
     
-    userEmails = []
-    for user in User.objects.all():
-        userEmails.append(user.email)
+    
     
     context = {
     'salesdata':salesdata,
     }
-
-    subject ="Issued ORWA :",order
-    sendfrom = settings.EMAIL_HOST_USER
-    to = ['tomt@pneumatrol.com']
-
-    #send_mail(subject,body,sendfrom,to,fail_silently=False,)
-    
-    text_content = 'Order issued. See pneumatrol live to view'
-    html_content  = render_to_string('ORWAapp/home/IssueEmail.html', context)
-
-    msg = EmailMultiAlternatives(subject, text_content, sendfrom, to)
-    msg.attach_alternative(html_content, "text/html")
-    msg.send()
 
     return HttpResponseRedirect(reverse('ORWAapp:home'))
     #return render(request,'ORWAapp/home/IssueEmail.html',context)
