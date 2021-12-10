ca4bb97df4f7b95a47d3130a2460e5baadab9bf2
tompt28 <tom_thornber@yahoo.co.uk>
2021-12-10 10:43:20 +0000

    logo update

--- pneumatrol_live/settings.py
+++ pneumatrol_live/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
-STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATIC_DIR = os.path.join(BASE_DIR, "ORWAapp/static")
 MEDIA_DIR = os.path.join(BASE_DIR, "media")
 
 
